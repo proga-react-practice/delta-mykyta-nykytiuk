8daeacbc4946c04f5e7fa90d360570bcc351784e
mykyta07 <mykytanyk@gmail.com>
2024-04-20 00:11:51 +0300

    render card on page

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,16 +2,27 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import "./App.css";
+import React from "react";
 import Form from "./Form";
+import Card from "./Card";
 
 function App() {
+  const [submittedData, setSubmittedData] = React.useState<FormData[]>([]);
   function handleSubmit(formData: FormData) {
     console.log(formData);
+    setSubmittedData([...submittedData, formData]);
   }
 
   return (
-    <div>
-      <Form onSubmit={handleSubmit} />
+    <div id="container">
+      <div id="formCar">
+        <Form onSubmit={handleSubmit} />
+      </div>
+      <div id="cardCar">
+        {submittedData.map((data, index) => (
+          <Card key={index} data={data} />
+        ))}
+      </div>
     </div>
   );
 }
