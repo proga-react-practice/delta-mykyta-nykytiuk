f6944d3ed4d87974368c9c6e9b83acde9a619ec1
mykyta07 <mykytanyk@gmail.com>
2024-04-24 18:44:13 +0300

    skipped folder

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import React from "react";
+import Form from "./componets/Form/Form";
+import Card from "./componets/Card/Card";
+
+function App() {
+  const [submittedData, setSubmittedData] = React.useState<FormData[]>([]);
+  function handleSubmit(formData: FormData) {
+    console.log(formData);
+    setSubmittedData([...submittedData, formData]);
+  }
+
+  function handleDelete(index: number) {
+    const newData = [...submittedData];
+    newData.splice(index, 1);
+    setSubmittedData(newData);
+  }
+
+  return (
+    <div id="container">
+      <div id="formCar">
+        <Form onSubmit={handleSubmit} />
+      </div>
+      <div id="cardCar">
+        {submittedData.map((data, index) => (
+          <Card key={index} data={data} onDelete={() => handleDelete(index)} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
