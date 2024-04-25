6cd7e030c443effa5a4f8e8faba5600332d130d5
mykyta07 <mykytanyk@gmail.com>
2024-04-25 11:50:20 +0300

    interfaces written in separate file

---
+++ src/interfaces.tsx
@@ -0,0 +1,13 @@
+export interface FormData {
+  brand: string;
+  model: string;
+  year: number | null;
+  body_type: string;
+  mileage_km: number;
+  gearbox: string;
+  fuel: string;
+  price_per_day: number;
+  horse_power: number;
+  engine_capacity: number;
+  purpose: string;
+}
